80e8567a32cecd68df44842799a4588322010b49
Trindkr <maria.trindkaer@gmail.com>
2022-01-05 12:03:58 +0100

    .

--- client/main.go
+++ client/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	gRPC "github.com/Trindkr/DistributedSystemsExam2022/proto"
@@ -27,17 +30,22 @@ func main() {
 	//parse flag/arguments
 	flag.Parse()
 
-	fmt.Println("--- CLIENT APP ---")
+	fmt.Println("=================================--Client--======================================")
 
-	//connect to log file
-	setLog()
+	//Setup method for the log.
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)//connect to log.txt file
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
 
-	fmt.Println("--- join Server ---")
+	fmt.Println("================= join Server =================")
 	ServerConn = make(map[gRPC.MessageServiceClient]*grpc.ClientConn) //Make map of server connections
 	joinServer()                                                      //Method call
-	defer closeAll()                                                  //when main method exits, close all the connections to the servers.
+	defer closeAll()       //when main method exits, close all the connections to the servers      
 
-	
+	parseValueFromClient()									   
 }
 
 func joinServer() {
@@ -52,67 +60,117 @@ func joinServer() {
 	defer cancel() //cancel the connection when we are done
 
 	for _, port := range _ports { //try to connect to all the ports in _ports with dial
-		log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, port)
+		log.Printf("Client %s: Attempts to dial on port %s\n", *clientsName, port)
 		conn, err := grpc.DialContext(timeContext, fmt.Sprintf(":%s", port), opts...) //dials the port with the given timeout
 		if err != nil {
-			log.Printf("Fail to Dial : %v", err)
+			log.Printf("Client %s: Fail to Dial : %v", *clientsName, err)//log error
 			continue
 		}
+		log.Printf("Client %s: connected to servre on port %s\n", *clientsName, port)//Log succesful connection to server
 		var s = gRPC.NewMessageServiceClient(conn)
 		servers = append(servers, s)          //add the new MessageServiceClient
 		ServerConn[s] = conn                  // maps the MessageServiceClient to its connection
-		fmt.Println(conn.GetState().String()) //prints connected if it's connected (i think (☞ﾟヮﾟ)☞)
+		fmt.Println(conn.GetState().String()) //prints connected if it's connected
 	}
 	ctx = context.Background()
 }
 
-// func parseInput() {
-// 	reader := bufio.NewReader(os.Stdin)
-// 	fmt.Println("Type the amount you wish to increment with here. Type 0 to get the current value")
-// 	fmt.Println("--------------------")
+func parseValueFromClient(){
 
-// 	//Infinite loop to listen for clients input.
-// 	for {
-// 		fmt.Print("-> ")
+	reader := bufio.NewReader(os.Stdin) //reads input from console.
+ 	fmt.Println("In order to insert a key value pair \n Type \"Put key val\" where key is your key and val is the value. ")
+ 	fmt.Printf("")
+	fmt.Println("In order to retrieve a value \n Type \"Get key \" where key is your key.")
+	fmt.Println("--------------------")
+	
+	//Infinite loop to listen for clients input.
+	for {
+		fmt.Print("-> ")
 
-// 		in, err := reader.ReadString('\n') //Read input into var in
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		in = strings.TrimSpace(in) //Trim input
-// 		incrementVal(in)
-// 	}
-// }
+		in, err := reader.ReadString('\n') //Read input into var in
+		if err != nil {
+			log.Fatal(err) //Log error
+		}
+		in = strings.TrimSpace(in) //Trim the clients input
+		splitInput:= strings.Split(in," ")
 
-// func incrementVal(in string) {
+		if strings.ToLower(splitInput[0]) == "get" {
+			GetValue(splitInput[1])
 
-// 	val, err := strconv.ParseInt(in, 10, 32) //Convert string to int64, return error if the int is larger than 32bit
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+		} else if strings.ToLower(splitInput[0]) == "put"{
+			PutValue(splitInput[1],splitInput[2])
 
-// 	//create amount type
-// 	amount := &gRPC.Amount{
-// 		ClientName: *clientsName,
-// 		Value:      int32(val), //cast from int64 to int32
-// 	}
-// 	for _, s := range servers {
-// 		if conReady(s) { //If the connection to the server is ready
-// 			fmt.Println(s)
-// 			ack, err := s.Increment(ctx, amount) //Make gRPC call to server with amount, and recieve acknowlegdement back.
-// 			if err != nil {
-// 				log.Printf("Client %s: no response from the server, attempting to reconnect", *clientsName)
-// 				log.Println(err)
-// 			}
-// 			if ack.NewValue >= val {
-// 				fmt.Printf("Success, the new value is now %d\n", ack.NewValue)
-// 			} else {
+		} else {
 
-// 				fmt.Println("Oh no something went wrong :(") //Hopefully this will never be reached
-// 			}
-// 		}
-// 	}
-// }
+			fmt.Println("Something went wrong, please ensure that your input is either a put or a get request.")
+		}
+	}
+
+}
+
+func GetValue(keyString string){
+
+	keyValue, err := strconv.ParseInt(keyString, 10, 32) //Convert string to int
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	//Create a instance of key type with the key value
+	key := &gRPC.Key{
+		Key: keyValue,
+	}
+
+	//This checks the connection to the servers before calling get method.
+	for _, s := range servers {
+		if conReady(s) { //If the connection to the server is ready
+			fmt.Println(s)
+			value, err := s.Get(ctx, key) //Make gRPC call to server with key, and recieve corrosponding value back.
+			if err != nil {
+				log.Printf("Client %s: no response from the server, attempting to reconnect", *clientsName)
+				log.Println(err)
+			}
+			log.Printf("Client %s: Succesful Get request with key: %d", *clientsName, keyValue)
+			fmt.Printf("Value: %d\n",value.Value) //Print the value in console.
+		}
+	}
+}
+
+func PutValue (keyString string, valueString string){
+
+	keyVal, err := strconv.ParseInt(keyString, 10, 32) //Convert string to int
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	valueVal, err := strconv.ParseInt(keyString, 10, 32) //Convert string to int
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	keyValue := &gRPC.KeyValue{
+		Key:   keyVal,
+		Value: valueVal,
+	}
+
+	//This checks the connection to the servers before calling get method.
+	for _, s := range servers {
+		if conReady(s) { //If the connection to the server is ready
+			fmt.Println(s)
+			con, err := s.Put(ctx, keyValue) //Make gRPC call to server with keyValue, and recieve confirmation.
+			if err != nil {
+				log.Printf("Client %s: no response from the server, attempting to reconnect", *clientsName)
+				log.Println(err)
+			}
+			if con.Confirmation{ //If confirmation bool is true print success
+				log.Printf("Client %s: Succesful Pet request with key: %d and value: %d", *clientsName, keyValue.Key,keyValue.Value)
+				fmt.Println("Put request was successful!")
+			}else{ //else print unsuccessful
+				log.Printf("Client %s: Unsuccesful Pet request with key: %d and value: %d", *clientsName, keyValue.Key,keyValue.Value)
+				fmt.Println("Put request unsuccessful, please try again!")
+			}
+		}
+	}
+}
 
 //Function which returns a true boolean if the connection to the server is ready, and false if it's not.
 func conReady(s gRPC.MessageServiceClient) bool {
@@ -123,16 +181,4 @@ func closeAll() {
 	for _, c := range ServerConn {
 		c.Close()
 	}
-}
-
-//Setup method for the log.
-func setLog() {
-
-	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
-	log.SetOutput(f)
-
 }
--- server/main.go
+++ server/main.go
@@ -1,1 +1,124 @@
 package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	gRPC "github.com/Trindkr/DistributedSystemsExam2022/proto"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	gRPC.UnimplementedMessageServiceServer 	//You need this line if you have a server
+	name string 							//Not required but useful if you want to name your server
+	port string 							//Not required but useful if your server needs to know what port it's listening to
+	mutex sync.Mutex 						//used to lock the server to avoid having multiple clients access the same resource at the same time.
+
+	keyValueMap map[int64]int64 			//Map to store keyvalue pairs.
+}
+
+var server *Server
+
+//flags used to get arguments from the terminal. Flags take a value, a default value and a description of the flag.
+var serverName = flag.String("name", "default", "Senders name") 
+var port = flag.String("port", "5400", "Server port")
+
+var _ports [5]string = [5]string{*port, "5401", "5402", "5403", "5404"} //Loops through the hardcoded ports to see if it can listen on one of them._ports
+
+
+func main() {
+	
+	setLog()
+
+	// This parses the flags and sets the correct/given corresponding values.
+	flag.Parse()
+	fmt.Println(".:server is starting:.")
+
+
+	go launchServer(_ports[:]) //starts a goroutine executing the launchServer method. Syntax note: [:] ensures that the entire array/slice is being sent.
+	
+	for {
+		time.Sleep(time.Second*5) //This makes sure that the main method is "kept alive"/keeps running
+	}
+}
+
+func (s *Server) Put(ctx context.Context, in *gRPC.KeyValue) (*gRPC.Confirmation, error) {
+
+	s.mutex.Lock()//locks the server ensuring no one else insert a key value pair into the map.
+	defer s.mutex.Unlock() //this unlocks the mutex when exiting the method
+
+	s.keyValueMap[in.Key]=in.Value
+
+	if s.keyValueMap[in.Key] != in.Value{
+		return &gRPC.Confirmation{Confirmation: false}, nil //If the value we just inserted into the map isn't there, return false
+	}
+	return &gRPC.Confirmation{Confirmation: true}, nil //else return true.
+	
+}
+
+func (s *Server) Get(ctx context.Context, in *gRPC.Key) (*gRPC.Value, error) { //Since we don't manipulate any data, no lock is needed here.
+	
+	var value int64 = s.keyValueMap[in.Key] //Get value from map
+	return &gRPC.Value{Value: value},nil //return value
+}
+
+func launchServer(ports []string) {
+	
+	log.Printf("Server %s: Attempts to create listener on port %s\n", *serverName, ports[0])
+
+	list, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", ports[0])) // Create listener tcp on given port or default port 5400
+	if err != nil {
+		log.Printf("Server %s: Failed to listen on port %s: %v", *serverName, *port, err) //If it fails to listen on the port, run launchServer method again with the next value/port in ports array
+		if len(ports) > 1 {
+			launchServer(ports[1:]) //[1:] makes a slice from item at index 1 to the last index, instead of taking all items in the array/slice. It exclude item at index 0.
+		} else {
+			log.Fatalf("Server %s: Failed to find open port", *serverName)//if it fails to listen on all ports, log error message and kill process.
+		}
+	}
+
+	var opts []grpc.ServerOption 			//Server options.
+	grpcServer := grpc.NewServer(opts...) 	//makes gRPC server 
+	server = newServer(ports[0]) 			//Item at index 0 is at this point the port which we succesfully listened to
+	gRPC.RegisterMessageServiceServer(grpcServer, server) //We think this method takes our own server and puts it into a grpc server, but again we not sure :)
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("failed to server %v", err)
+	}
+	//code here is unreachable because serve occupies the current thread.
+}
+
+//creates a new instance of a server type
+func newServer(serverPort string) *Server {
+	s := &Server{
+		name: *serverName, 		//* is used because of flags
+		port: serverPort, 		// not sure why * isnt used here but it isn't
+		keyValueMap: make(map[int64]int64), 		// initialize a map. If no value exists in the map, the default value is 0, ensuring propery 2.
+	}
+
+	fmt.Println(s) 				//prints the server struct to console
+	return s 					//return server
+}
+
+func setLog(){
+	//Clears the log.txt file when a new server is started
+	//This is used for logging information to a log file.
+	if err := os.Truncate("log.txt", 0); err != nil {
+		log.Printf("Failed to truncate: %v", err)
+	}
+
+	//This connects to the log file/changes the output of the log informaiton to the log.txt file.
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+}
